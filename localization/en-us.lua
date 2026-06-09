66e09b4cbe5d066d49ce8721a9113c4c281e6f31
Lunae <florentsfloweringblossom@hotmail.com>
2026-06-09 13:47:11 -0500

    More Glowing Cards
    Added three new glowing cards
    Defunding
    Lab Grown
    Shutdown

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -29,6 +29,30 @@ return {
                     '{C:green}#1#/#2#{} chance to {C:attention}destroy{} each {C:attention}card{}',
                     'of the {C:hearts}Hearts{} suit.'
                 }
+            },
+            c_afterglow_Shutdown = {
+                name = 'Shutdown',
+                text = {
+                    'If there are any {C:clubs}Clubs{} in deck,',
+                    '{C:green}#1#/#2#{} chance to {C:attention}destroy{} each {C:attention}card{}',
+                    'of the {C:clubs}Clubs{} suit.'
+                }
+            },
+            c_afterglow_LabGrown = {
+                name = 'Lab Grown',
+                text = {
+                    'If there are any {C:diamonds}Diamonds{} in deck,',
+                    '{C:green}#1#/#2#{} chance to {C:attention}destroy{} each {C:attention}card{}',
+                    'of the {C:diamonds}Diamonds{} suit.'
+                }
+            },
+            c_afterglow_Defunding = {
+                name = 'Defunding',
+                text = {
+                    'If there are any {C:spades}Spades{} in deck,',
+                    '{C:green}#1#/#2#{} chance to {C:attention}destroy{} each {C:attention}card{}',
+                    'of the {C:spades}Spades{} suit.'
+                }
             }
         },
         Voucher = {
---
+++ src/Consumables/Defunding.lua
@@ -0,0 +1,64 @@
+SMODS.Consumable {
+    key = 'Defunding',
+    atlas = 'consumables',
+    pos = {
+        x = 2,
+        y = 0
+    },
+    config = {
+        card_limit = 0,
+        extra = { odds_num = 2, odds_den = 3 }
+    },
+    set = 'glowing',
+    hidden = false,
+    soul_set = 'Tarot',
+    soul_rate = 0.5,
+
+    loc_vars = function(self, info_queue, card)
+        local num, den = SMODS.get_probability_vars(card, self.config.extra.odds_num, self.config.extra.odds_den, 'heartbreaker_destroy_odds')
+        return { vars = { num, den } }
+    end,
+
+    use = function(self, card, area, copier)
+        local hearts_in_deck = {}
+        for _, deck_card in ipairs(G.playing_cards) do
+            if deck_card.ability.set == 'Default' and deck_card:is_suit('Spades') then
+                table.insert(hearts_in_deck, deck_card)
+            end
+        end
+
+        local cards_to_destroy = {}
+
+        for _, heart_card in ipairs(hearts_in_deck) do
+            if SMODS.pseudorandom_probability(card, 'heartbreaker_destroy', self.config.extra.odds_num, self.config.extra.odds_den, 'heartbreaker_destroy_odds') then
+                table.insert(cards_to_destroy, heart_card)
+            end
+        end
+
+        if #cards_to_destroy > 0 then
+            G.E_MANAGER:add_event(Event({
+                trigger = 'immediate',
+                func = function()
+                    for _, target_card in ipairs(cards_to_destroy) do
+                        target_card:juice_up()
+                        target_card:start_dissolve()
+                    end
+                    return true
+                end
+            }))
+        end
+    end,
+    
+    can_use = function(self, card)
+        if not G.playing_cards then return false end
+        for _, deck_card in ipairs(G.playing_cards) do
+            if deck_card.ability.set == 'Default' then
+                if deck_card:is_suit('Spades') then
+                    return true
+                end
+            end
+        end
+
+        return false
+    end
+}
